f5e17f0fda4f1936bfddc81bdc6c223ac3c09a5e
Munnos <munnos@hotmail.co.uk>
2022-09-10 13:53:04 +0100

    test for employee almost complete

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const inquirer = require("inquirer)");
+const inquirer = require("inquirer");
 const fs = require('fs');
+const Employee = require("./lib/Employee");
+
+
 
--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -17,24 +17,42 @@ describe("employee", () => {
 
     it("should set name via constructor arguments", () => {
       const name = "michael";
-      const e = new Employee(name);
+      const e = new Employee(name,undefined,undefined);
       expect(e.name).toBe("michael");
+      
     });
 
 
   it("should set id via constructor arguments", () => {
       const id = "45678";
-      const e = new Employee(id);
+      const e = new Employee(undefined,id,undefined);
       expect(e.id).toBe("45678");
+      console.log(Employee);
+      
     });
+
+    
 
       it("should set email via constructor arguments", () => {
         const email = "josephdesirejob@ntl.com";
-        const e = new Employee(email);
+        const e = new Employee(undefined,undefined,email);
         expect(e.email).toBe("josephdesirejob@ntl.com");
       });
     });
-});
+
+    it("function getName() should return given name", () => {
+      const name = "michael";
+      const id = "45678";
+      const email = "josephdesirejob@ntl.com";
+      // const e = new Employee (name, id, email);
+      const e = new Employee (name, id, email);
+      const spy = jest.spyOn(e, "getName").mockImplementation(() => 'michael');
+      expect(e.getName()).toBe("michael");
+
+    })})
+
+
+
 
 
 // describe(

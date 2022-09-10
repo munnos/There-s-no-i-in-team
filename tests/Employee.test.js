e90f618cd01ab509692a8edc4325b8a9f8bf5d68
Munnos <munnos@hotmail.co.uk>
2022-09-10 11:16:33 +0100

    troubleshoot tests

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -2,43 +2,65 @@ const Employee = require("../lib/Employee");
 // const e = new Employee;
 
 describe("employee", () => {
+  describe("Initialisation", () => {
   it("should create employee object", () => {
     const e = new Employee();
     expect(typeof e).toEqual("object");
   });
-});
 
-describe("constructor argument - name", () => {
+// it("should return an object containing a 'string' property when called with the 'new' keyword", () => {
+//   const e = new Employee();
+
+//   expect(e).toContain(email, id);
+
+// });
+
     it("should set name via constructor arguments", () => {
       const name = "michael";
       const e = new Employee(name);
       expect(e.name).toBe("michael");
     });
-  });
 
-  describe("constructor argument - id", () => {
-    it("should set id via constructor arguments", () => {
+
+  it("should set id via constructor arguments", () => {
       const id = "45678";
       const e = new Employee(id);
       expect(e.id).toBe("45678");
     });
-  });
 
-  describe("constructor argument - email", () => {
-    it("should set email via constructor arguments", () => {
-      const email = "josephdesirejob@ntl.com";
-      const e = new Employee(email);
-      expect(e.email).toBe("josephdesirejob@ntl.com");
+      it("should set email via constructor arguments", () => {
+        const email = "josephdesirejob@ntl.com";
+        const e = new Employee(email);
+        expect(e.email).toBe("josephdesirejob@ntl.com");
+      });
     });
-  });
+});
 
-  describe("function - getRole", () => {
-    it("should return role as string employee", () => {
-      const e = newEmployee;
-      getRole();
-      expect(getRole()).toBe("Employee");
-    })
-  });
+
+// describe(
+
+
+
+
+
+
+  // describe("constructor argument - id", () => {
+  //   it("should set id via constructor arguments", () => {
+  //     const id = "45678";
+  //     const e = new Employee(id);
+  //     expect(e.id).toBe("45678");
+  //   });
+  // });
+
+  
+
+  // describe("function - getRole", () => {
+  //   it("should return role as string employee", () => {
+  //     const e = newEmployee;
+  //     getRole();
+  //     expect(getRole()).toBe("Employee");
+  //   })
+  // });
 
 
     
